d9cab61d8e9258d7babfc232c9bb2d6310ea8181
Glenn Inn <glenninn@yahoo.com>
2017-11-06 09:45:00 -0800

    Componentize, update Readme
    Separate the main application and the BlueJeans event code for
    readability.
    Update the Readme: fix typos, add some additional info about the app

--- README.md
+++ README.md
@@ -4,9 +4,14 @@
 
 
 
-## Reference Desigh for Monitoring a BlueJeans Meting
+## Reference Design for Monitoring BlueJeans Meeting Events
 
 - 11/6/2017, g1, Initial check-in
+
+This Node JS application demonstrates how to employ the BlueJeans' Events SDK to enable an application to monitor the Video and Audio mute states for each participant in a meeting.
+
+The meeting you wish to monitor must be scheduled without requiring an attendee passcode.  The application connects using a meeting access token obtained on behalf the organizer's id.
+
 
 ### To Install
 1. Download the files from this git repository
@@ -14,5 +19,6 @@
 
 ### To Run
 The application is contained in the eventmon.js file.  It requires one parameter when launching:  the numeric ID of the BlueJeans meeting
+
 
 `node eventmon numeric_id`
--- eventmon.js
+++ eventmon.js
@@ -20,351 +20,7 @@ var _ = require('underscore');
 var my = require('myclass');
 var sockjs = require('sockjs-client');
 
-
-// --------------------------------------------------------------
-//       The BlueJeans Event Handler Object
-// --------------------------------------------------------------
-
-function eventService(_, my, sockjs)
-{
-    var invokeIfImplemented = function(collection, methodName, arg)
-    {
-        return _.invoke(_.filter(collection, function (item)
-        {
-            return item[methodName] !== undefined;
-        }), methodName, arg);
-    };
-
-    var EventService = my.Class(
-    {
-        events: function()
-        {
-            return {
-                "guid_assigned": this.guidAssigned,
-                "remoteclose": this.remoteclose,
-                "pairingError": this.pairingError,
-                "kicked": this.kicked
-            };
-        },
-
-        maxReconnects: 10,
-
-        reconnects: 0,
-
-        reconnectBackoff: 1000,
-
-        constructor: function()
-        {
-            this.handlers = {};
-        },
-
-        registerHandler: function(handler, namespace, customOpts)
-        {
-            this.handlers[namespace] = handler;
-        },
-
-        setUpSocket: function(options, reconnect_count)
-        {
-            var self = this;
-            var sock_url = options.eventServiceUrl || '';
-
-            if (self.sock)
-            {
-                delete self.connected;
-                if (self.joinTimeout)
-                {
-                    clearTimeout(self.joinTimeout);
-                    delete self.joinTimeout;
-                }
-
-                self.sock.onclose = function()
-                {
-                    // Dummy function to avoid reconnect in the onclose method
-                    // of previous socket connection.
-                };
-            }
-
-            self.close(); //prevent multiple connections
-
-            self.options = options;
-            self.meetingAccessToken = options.access_token;
-
-            var sockjs_protocols = [
-                    'websocket', 'xdr-streaming', 'xhr-streaming',
-                    'xdr-polling', 'xhr-polling', 'iframe-xhr-polling',
-                    'jsonp-polling'
-            ];
-            
-            var sock = self.sock = new sockjs(sock_url, {},
-            {
-                cookie: true,
-                transports: sockjs_protocols
-            });
-
-            if (self.joinTimeout)
-            {
-                clearTimeout(self.joinTimeout);
-                delete self.joinTimeout;
-            }
-
-            sock.onopen = function()
-            {
-                sock._selfclosed = false;
-                sock._remoteclosed = false;
-
-                if(self._crashed){
-                    sock.close();
-                    return;
-                }
-                 
-                options.events = ['meeting', 'endpoint'];
-                self.sendEvent('meeting.register', options);
-                invokeIfImplemented(_.values(self.handlers), "onOpen", self.meetingAccessToken);
-                self.reconnects = 0;
-                if (reconnect_count && reconnect_count > 0)
-                {
-                    //window.Notifications.trigger('socket:reconnected');
-                }
-
-                self.joinTimeout = setTimeout(function()
-                {
-                   if(!self.connected)
-                   {
-                       self.sock.close();
-                   }
-                   delete self.joinTimeout;
-                },10000);
-            };
-
-            sock.onmessage = function(_e)
-            {
-				var msg;
-				try{
-					msg = JSON.parse(_e.data);
-				} catch(e) {
-					conGoto(1,1,"Parse error");
-				}
-              //  try
-                {
-                    if ((msg.length == 2) && (typeof msg[1] === 'object'))
-                    {
-                        var evt = msg[0];
-						conGoto(15,1,evt+ conEraEOL);
-                        switch(evt)
-                        {
-                            case 'keepalive':
-                                self.sendEvent("heartbeat");
-                                break;
-                            default:
-                              var evt_data = msg[1];
-                              if(evt_data && evt_data.reqId && self.reqCallbacks[evt_data.reqId])
-                              {
-                                var cb = self.reqCallbacks[evt_data.reqId];
-                                delete self.reqCallbacks[evt_data.reqId];
-                                cb(evt_data.error,evt_data.data);
-                                break;
-                              }
-
-                              var protocolEvent = evt.match("([^.]*)$")[0];
-							  
-                              if (protocolEvent in self.events())
-                              {
-                                  //self.events()[protocolEvent](evt_data);
-                                  var c = self.events()[protocolEvent];
-                                  c.call(self, evt_data);
-                              }
-                              else
-                              {
-                                  var namespaces = _.keys(self.handlers);
-                                  var eventNamespace = _.find(namespaces, function (namespace)
-                                  {
-                                      return evt.match("^"+namespace);
-                                  });
-
-                                  self.handlers[eventNamespace].onMessage(evt, evt_data);
-                              }
-
-                              break;
-                        }
-                    }
-                    else
-                    {
-                        errMsg("JSON Received but not valid event: " + (msg[0] || ""));
-                    }
-                }
-               /* catch (e)
-                {
-                    // console.log("ERROR: " + e)
-                    //invalid json, discarding
-                    errMsg("Error: Invalid JSON from SockJS - " + JSON.stringify(e));
-                }*/
-            };
-
-            sock.onclose = function()
-            {
-                delete self.connected;
-
-                if (self.joinTimeout)
-                {
-                    clearTimeout(self.joinTimeout);
-                    delete self.joinTimeout;
-                }
-                if (
-                    !self.sock._selfclosed &&
-                    !self.sock._remoteclosed &&
-                    !self._crashed &&
-
-                    !self._timeoutClosed &&
-                    !self._kicked
-
-                    ) {
-                    invokeIfImplemented(_.values(self.handlers), "onClosedUnexpectedly", {});
-                    self.reconnect();
-                }
-                else
-                {
-                    invokeIfImplemented(_.values(self.handlers), "onClose", {});
-                    //window.Notifications.trigger('socket:closed');
-                }
-                //Logger.warn("SockJS connection closed");
-            };
-            sock.onerror = function(e) {
-                //Logger.warn("SockJS error occured");
-                invokeIfImplemented(_.values(self.handlers), "onError", {});
-            };
-        },
-		
-        guidAssigned: function(event)
-        {
-            // console.log("Connected to event service. Endpoint guid: " + event.seamGuid + ", chat guid: " + event.guid);
-			conGoto(statRow1,1,conReset+"(Evt Svc: connected) Endpt guid: " + event.seamGuid );
-			
-            this.connected = true;
-            //cofa.skinny.instances.selfParticipant.set({id: event.seamGuid});
-            //invokeIfImplemented(_.values(this.handlers), "onConnect");
-            //window.Notifications.trigger('socket:connected');
-        },
-
-        close: function()
-        {
-            this.connected = false;
-            if (this.sock)
-            {
-                invokeIfImplemented(_.values(this.handlers), "onClose", {});
-                //Logger.info("Closing SockJS connection");
-                this.sock._selfclosed = true;
-                this.sock.close();
-            }
-        },
-
-        reconnect: function()
-        {
-            errMsg("Reconnect!")
-            var self = this;
-            this.connected = false;
-            if (self.sock._remoteclosed) return;
-            if (self.sock._kicked) return;
-            if (self._timeoutClosed) return;
-            if (self.reconnects < self.maxReconnects && self.meetingAccessToken && !self._reconnecting)
-            {
-                //window.Notifications.trigger('socket:reconnecting');
-                self._reconnecting = true;
-                setTimeout(function()
-                {
-                    errMsg("Reconnecting");
-                    self.setUpSocket(self.options, self.reconnects);
-                    self._reconnecting = false;
-                    self.reconnects++;
-                }, self.reconnectBackoff * (self.reconnects > 10 ? 10 : self.reconnects));
-            }
-        },
-
-        remoteclose: function()
-        {
-            errMsg("remote close")
-            var self = this;
-            self.sock._remoteclosed = true;
-            invokeIfImplemented(_.values(self.handlers), "remoteclose");
-        },
-
-        pairingError: function(error)
-        {
-            var self = this;
-            errMsg("Error Pairing Meeting: "+ JSON.stringify(error));
-            setTimeout(function()
-            {
-                self.sock.close();
-            }, 200);
-        },
-
-        isDisconnected: function()
-        {
-            return !this.isConnected();
-        },
-
-        isConnected: function()
-        {
-            return this.sock && this.connected;
-        },
-
-        isJoinEvent: function(eventName)
-        {
-            return eventName === 'meeting.register';
-        },
-
-        sendEvent: function(event_name, event_data)
-        {
-            if (event_name === 'heartbeat' || this.isJoinEvent(event_name) || this.isConnected())
-            {
-                this.sock.send(JSON.stringify([event_name, event_data || {}]));
-            }
-            else
-            {
-                errMsg("Cant send event yet -- sock or guid not ready");
-            }
-        },
-
-        sendRequest: function(event_name, event_data, callback)
-        {
-          if(this.isConnected())
-          {
-            if(!this.reqId)
-            {
-              this.reqId = 1;
-            }
-            else 
-            {
-              this.reqId++;
-            }
-            if(!this.reqCallbacks)
-            {
-              this.reqCallbacks = {};
-            }
-            this.reqCallbacks[this.reqId] = callback;
-            this.sock.send(JSON.stringify([event_name, {reqId: this.reqId, data: (event_data || {})}]));
-          } else {
-            callback({error: {message: "Sending request while not connected."}});
-          }
-        },
-
-        kicked: function(event)
-        {
-            errMsg("Kicked");
-            this.sock._remoteclosed = true;
-            this.sock._kicked = true;
-            this.sock.close();
-        },
-
-        crashed: function()
-        {
-            errMsg("Crashed");
-            this._crashed = true;
-            this._idleTimeout();
-        }
-    });
-
-    return new EventService();
-}
+var evtModule = require('./eventService');
 
 
 
@@ -376,9 +32,9 @@ var roster_names = {};
 var party = [];
 
 
-var statRow = 23;
-var statRow1 = statRow + 1;
-var errRow = statRow+2;
+var titleRow= 22;
+var statRow = titleRow + 1;
+var errRow = titleRow+2;
 
 var colName = 1;
 var colCnct = 30;
@@ -397,7 +53,6 @@ var conBgYellow = "\x1b[43m";
 var conFgWhite = "\x1b[37m";
 var conTitle = "\x1b[37;44m";
 var conEraEOP= "\x1b[J";
-var conEraEOL = "\x1b[K";
 
 
 
@@ -405,13 +60,17 @@ function errMsg(msg){
 	conGoto(errRow,1,msg);
 }
 
+function statusMsg(msg){
+	conGoto(statRow,1,msg);
+};
+
 function conGoto(x,y,msg){
 	console.log("\x1b["+x+";"+y+"H" + msg );
 }
 
 function conClrScrn(){
 	conGoto(1,1,conEraEOP);
-	conGoto(1,colName,conTitle+"Name");
+	conGoto(1,colName,conTitle+"Participant");
 	conGoto(1,colCnct,conTitle+"Meeting");
 	conGoto(1,colVmute,conTitle+"Video");
 	conGoto(1,colAmute,conTitle+"Audio"+conReset);
@@ -635,12 +294,11 @@ var handler =
     }
 };
 
-var 
- oauthRec = {
+var oauthRec = {
 	 grant_type :"meeting_passcode",
 	 meetingNumericId : meeting_id,
 	 meetingPasscode : ""
- };
+};
 var uri = "api.bluejeans.com";
 var authPath = "/oauth2/token?meeting_passcode";
 
@@ -656,7 +314,7 @@ auth.post( uri, authPath,oauthRec).then(function(results){
 	// --------------------------------------------------------------
 	// 				Instantiation of My Event Handler
 	// --------------------------------------------------------------
-	var myMeetingEvents = eventService(_, my, sockjs);
+	var myMeetingEvents = evtModule.eventService(_, my, sockjs);
 	if (myMeetingEvents)
 	{
 		 var opts =
@@ -674,10 +332,11 @@ auth.post( uri, authPath,oauthRec).then(function(results){
 		};
 
 		myMeetingEvents.setUpSocket(opts);
+		myMeetingEvents.setStatusCallbacks(statusMsg,errMsg);
 		myMeetingEvents.registerHandler(handler, 'meeting.register.error');   
 		myMeetingEvents.registerHandler(handler, 'meeting.notification');   
 	}
-    conGoto(statRow,1,conReset+"Events Monitor for meeting: "+ meeting_id);
+    conGoto(titleRow,1,conReset+"Events Monitor for meeting: "+ meeting_id);
 
 },function(errors){
 	console.log("Error!: " + errors);
